7cbd2339fdf50465accd7494b28b9173e02716b2
Maria1504bu <15504420011m@gmail.com>
2023-02-21 11:22:21 +0200

    create skeleton for sushi block

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import SushiBlock from './components/SushiBlock'; 
+import Skeleton from './components/SushiBlock/Skeleton';
 
 import './scss/app.scss';
 
 function App() {
   const [sushi, setSushi] = React.useState([]);
+  const [loaded, setLoaded] = React.useState(false);
   
   React.useEffect(() => {
     fetch("https://63f3a4c5de3a0b242b46ab95.mockapi.io/items")
     .then((res) => res.json())
-    .then((items) => setSushi(items))
+    .then((items) => {
+      setSushi(items);
+      setLoaded(true);})
   }, [])
     return (
       <div className='wrapper'>
@@ -25,9 +29,8 @@ function App() {
             </div>
             <h2 className='content__title'>Всі суші</h2>
             <div className='content__items'>
-              {sushi.map(obj => (
-                <SushiBlock key={obj.id} {...obj} />
-              ))}
+              {loaded ? sushi.map(obj => (<SushiBlock key={obj.id} {...obj} />
+              )) :[... new Array(4)].map(() => <Skeleton />)}
             </div>
           </div>
         </div>
---
+++ src/components/SushiBlock/Skeleton.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const MyLoader = (props) => (
+  <ContentLoader className="sushi-block"
+    speed={2}
+    width={280}
+    height={379.5}
+    viewBox="0 0 280 379.5"
+    backgroundColor="#e8f8f1"
+    foregroundColor="#ffecd1"
+    {...props}
+  >
+    <circle cx="130" cy="98" r="80"/> 
+    <rect x="14" y="210" rx="0" ry="0" width="260" height="27.2" /> 
+    <rect x="14" y="260" rx="10" ry="10" width="260" height="46" /> 
+    <rect x="20" y="335" rx="5" ry="5" width="80" height="35" /> 
+    <rect x="123" y="330" rx="21" ry="21" width="142" height="45" />
+  </ContentLoader>
+)
+
+export default MyLoader
