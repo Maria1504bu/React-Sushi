75a067e846162798823f8ab40e90054c78e92428
Maria1504bu <15504420011m@gmail.com>
2023-03-05 22:49:09 +0200

    create map of cities

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { Routes, Route } from "react-router-dom";
 
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
-import ItemPage from "./pages/ItemPage";
+import ItemPage from "./pages/FullItem";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 
 import "./scss/app.scss";
+import MapOfUkraine from "./pages/MapOfUkraine";
 
 function App() {
   const ghRepo = "/React-Sushi"
@@ -19,6 +20,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path= {`${ghRepo}/`} element={<Home />} />
+            <Route path={`${ghRepo}/Map`} element={<MapOfUkraine />} />
             <Route path= {`${ghRepo}/:id`} element={<ItemPage />} />
             <Route path={`${ghRepo}/Cart`} element={<Cart />} />
             <Route path="*" element={<NotFound />} />
